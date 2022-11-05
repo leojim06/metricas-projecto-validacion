86ab29c1fea65017a65ddbcec2d2d4c4af233fc8
o.jimenez-at-285254948890 <leojim06@gmail.com>
2022-11-05 12:35:52 -0500

    eliminar archivos para evitar analisis

--- scripts/cultura-gastronomica-receta.sql
+++
@@ -1,2 +0,0 @@
-UPDATE receta_entity
-SET "culturaGastronomicaId" = '7333d139-b131-479e-ae2b-c7adf63bf5b2'
--- scripts/cultura-gastronomica-restaurante.sql
+++
@@ -1,7 +0,0 @@
-INSERT into cultura_gastronomica_entity_restaurantes_restaurante_entity
-SELECT (
-select cultura_gastronomica_entity.id
-  from cultura_gastronomica_entity
- order by id asc
- limit 1), restaurante_entity.id
-FROM restaurante_entity;
